15202db818cef02a6439de08633018d5ffffae3a
Arpit <arpitvaish@hotmail.com>
2018-11-02 08:42:49 +0530

    Create Reverse a linked list.java

---
+++ Data Structures/Reverse a linked list.java
@@ -0,0 +1,105 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+	static class SinglyLinkedListNode {
+		public int data;
+		public SinglyLinkedListNode next;
+
+		public SinglyLinkedListNode(int nodeData) {
+			this.data = nodeData;
+			this.next = null;
+		}
+
+		@Override
+		public String toString() {
+			return "" + data;
+		}
+	}
+
+	static class SinglyLinkedList {
+		public SinglyLinkedListNode head;
+		public SinglyLinkedListNode tail;
+
+		public SinglyLinkedList() {
+			this.head = null;
+			this.tail = null;
+		}
+
+		public void insertNode(int nodeData) {
+			SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+			if (this.head == null) {
+				this.head = node;
+			} else {
+				this.tail.next = node;
+			}
+
+			this.tail = node;
+		}
+	}
+
+	public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter)
+			throws IOException {
+		while (node != null) {
+			bufferedWriter.write(String.valueOf(node.data));
+
+			node = node.next;
+
+			if (node != null) {
+				bufferedWriter.write(sep);
+			}
+		}
+	}
+
+	static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		SinglyLinkedListNode node = reverse(head.next);
+		// head.next = null;
+		head.next.next = head;
+		head.next = null;
+
+		return node;
+
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		// BufferedWriter bufferedWriter = new BufferedWriter(new
+		// FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int tests = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int testsItr = 0; testsItr < tests; testsItr++) {
+			SinglyLinkedList llist = new SinglyLinkedList();
+
+			int llistCount = scanner.nextInt();
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+			for (int i = 0; i < llistCount; i++) {
+				int llistItem = scanner.nextInt();
+				scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+				llist.insertNode(llistItem);
+			}
+
+			SinglyLinkedListNode llist1 = reverse(llist.head);
+
+			// printSinglyLinkedList(llist1, " ", bufferedWriter);
+			// bufferedWriter.newLine();
+		}
+
+		// bufferedWriter.close();
+
+		scanner.close();
+	}
+}
