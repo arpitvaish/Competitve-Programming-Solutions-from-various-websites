818ff4f1717ef1e7c69493ac4e663efe04325ffd
Arpit <arpitvaish@hotmail.com>
2018-11-04 09:16:17 +0530

    Create Delete duplicate-value nodes from a sorted linked list.java

---
+++ Data Structures/Delete duplicate-value nodes from a sorted linked list.java
@@ -0,0 +1,105 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+
+	static class SinglyLinkedListNode {
+		public int data;
+		public SinglyLinkedListNode next;
+
+		public SinglyLinkedListNode(int nodeData) {
+			this.data = nodeData;
+			this.next = null;
+		}
+
+		public String toString() {
+			return "" + data;
+		}
+	}
+
+	static class SinglyLinkedList {
+		public SinglyLinkedListNode head;
+		public SinglyLinkedListNode tail;
+
+		public SinglyLinkedList() {
+			this.head = null;
+			this.tail = null;
+		}
+
+		public void insertNode(int nodeData) {
+			SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+			if (this.head == null) {
+				this.head = node;
+			} else {
+				this.tail.next = node;
+			}
+
+			this.tail = node;
+		}
+	}
+
+	public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter)
+			throws IOException {
+		while (node != null) {
+			bufferedWriter.write(String.valueOf(node.data));
+
+			node = node.next;
+
+			if (node != null) {
+				bufferedWriter.write(sep);
+			}
+		}
+	}
+
+	static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head) {
+
+		if (head == null)
+			return null;
+		SinglyLinkedListNode nextItem = head.next;
+		while (nextItem != null && head.data == nextItem.data) {
+			nextItem = nextItem.next;
+		}
+		head.next = removeDuplicates(nextItem);
+		return head;
+
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		// BufferedWriter bufferedWriter = new BufferedWriter(new
+		// FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int t = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int tItr = 0; tItr < t; tItr++) {
+			SinglyLinkedList llist = new SinglyLinkedList();
+
+			int llistCount = scanner.nextInt();
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+			for (int i = 0; i < llistCount; i++) {
+				int llistItem = scanner.nextInt();
+				scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+				llist.insertNode(llistItem);
+			}
+
+			SinglyLinkedListNode llist1 = removeDuplicates(llist.head);
+
+			// printSinglyLinkedList(llist1, " ", bufferedWriter);
+
+			// bufferedWriter.write(String.valueOf(result));
+			// bufferedWriter.newLine();
+		}
+
+		// bufferedWriter.close();
+
+		scanner.close();
+	}
+}
